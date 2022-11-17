34c3fb926ff17c0e6253bcfbedc5bc4e19d3303f
KARTHIKEYA <Surak504@gmail.com>
2022-11-18 00:02:24 +0530

    ⬇️ dev test sample

--- src/context.js
+++ src/context.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect, useContext } from "react";
 
+import sample from "./assets/sample.json";
+
 export const API_ENDPOINT = `https://api.covalenthq.com/v1/1/xy=k/uniswap_v2/tokens/address/${process.env.REACT_APP_CONTRACT_ADDRESS}/transactions/?key=${process.env.REACT_APP_COVALENTHQ_API_KEY}&format=JSON`;
 
 const AppContext = React.createContext();
@@ -37,6 +39,14 @@ const useFetch = (urlParams) => {
   return { isLoading, error, recordsList };
 };
 
+const useSample = () => {
+  const isLoading = false;
+  const error = { show: false, msg: "" };
+  const recordsList = sample.items;
+
+  return { isLoading, error, recordsList };
+};
+
 const AppProvider = ({ children }) => {
   const [currency, setCurrency] = useState("USD");
   const [itemSize, setItemSize] = useState(5);
@@ -44,6 +54,8 @@ const AppProvider = ({ children }) => {
   const { isLoading, error, recordsList } = useFetch(
     `&quote-currency=${currency}&page-size=${itemSize}`
   );
+
+  // const { isLoading, error, recordsList } = useSample()
 
   return (
     <AppContext.Provider
