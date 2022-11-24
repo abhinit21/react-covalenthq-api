ed8025df3751d6c7ba1cc46e7fce471f9eab8477
KARTHIKEYA <Surak504@gmail.com>
2022-11-24 17:00:10 +0530

    ✨ design

--- src/components/TableList.js
+++ src/components/TableList.js
@@ -1,7 +1,9 @@
 import React from "react";
-import { Table } from "react-bootstrap";
+import { Card, Table, ButtonGroup, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { GrInfo } from "react-icons/gr";
+import { IconContext } from "react-icons";
+import { FaChevronRight, FaChevronLeft } from "react-icons/fa";
 
 import Loading from "./Loading";
 import { useGlobalContext } from "../context";
@@ -18,36 +20,74 @@ function TableList() {
   }
 
   return (
-    <Table responsive>
-      <thead>
-        <tr>
-          <th></th>
-          <th>tx_hash</th>
-          <th>from_address</th>
-          <th>to_address</th>
-          <th>gas_price</th>
-          <th>value_quote</th>
-        </tr>
-      </thead>
-      <tbody>
-        {recordsList.map((record) => {
-          return (
-            <tr key={record.tx_hash}>
-              <td>
-                <GrInfo />
-              </td>
-              <td>
-                <Link to={`record/${record.tx_hash}`}>{record.tx_hash}</Link>
-              </td>
-              <td>{record.from_address}</td>
-              <td>{record.to_address}</td>
-              <td>{record.gas_price}</td>
-              <td>{record.value_quote}</td>
+    <main className="p-4">
+      <Card style={{ border: "none" }} className="shadow px-3">
+        <div className="d-flex justify-content-between">
+          <ButtonGroup
+            style={{ height: "2.5rem" }}
+            className="d-flex gap-2 rounded-5 my-3"
+          >
+            <Button className="rounded">First</Button>
+            <div className="bg-primary rounded-3 px-2">
+              <IconContext.Provider value={{ color: "white" }}>
+                <div className="pt-2 ps-1">
+                  <FaChevronRight fontSize="1.5rem" />
+                </div>
+              </IconContext.Provider>
+            </div>
+            <p
+              style={{ fontSize: "1rem", height: "2.5rem" }}
+              className="bg-primary rounded-3 p-2 text-white"
+            >
+              Page 1 of 5
+            </p>
+            <div className="bg-primary rounded-3 px-2">
+              <IconContext.Provider value={{ color: "white" }}>
+                <div className="pt-2 pe-1">
+                  <FaChevronLeft fontSize="1.5rem" />
+                </div>
+              </IconContext.Provider>
+            </div>
+            <Button className="rounded">Last</Button>
+          </ButtonGroup>
+          <Button style={{ width: "6rem" }} className="rounded-5 my-3">
+            View All
+          </Button>
+        </div>
+        <Table responsive>
+          <thead>
+            <tr>
+              <th></th>
+              <th>tx_hash</th>
+              <th>from_address</th>
+              <th>to_address</th>
+              <th>gas_price</th>
+              <th>value_quote</th>
             </tr>
-          );
-        })}
-      </tbody>
-    </Table>
+          </thead>
+          <tbody>
+            {recordsList.map((record) => {
+              return (
+                <tr key={record.tx_hash}>
+                  <td>
+                    <GrInfo />
+                  </td>
+                  <td>
+                    <Link to={`record/${record.tx_hash}`}>
+                      {record.tx_hash}
+                    </Link>
+                  </td>
+                  <td>{record.from_address}</td>
+                  <td>{record.to_address}</td>
+                  <td>{record.gas_price}</td>
+                  <td>{record.value_quote}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      </Card>
+    </main>
   );
 }
 
